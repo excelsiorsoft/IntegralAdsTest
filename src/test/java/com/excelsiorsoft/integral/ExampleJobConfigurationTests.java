553ad2d1a051f4a6f96133b2d93c65944f4ca2fa
sleyzerzon <senya22@yahoo.com>
2014-09-06 23:45:47 -0400

    cleaning up

--- src/test/java/com/excelsiorsoft/integral/ExampleJobConfigurationTests.java
+++ src/test/java/com/excelsiorsoft/integral/ExampleJobConfigurationTests.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertNotNull;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.Job;
@@ -16,11 +15,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import akka.actor.Props;
 
 import com.excelsiorsoft.integral.process.ExampleConfiguration;
-import com.excelsiorsoft.integral.process.akka.Master;
 import com.excelsiorsoft.integral.process.akka.Result;
 
 @ContextConfiguration(locations={"/launch-context.xml"})
